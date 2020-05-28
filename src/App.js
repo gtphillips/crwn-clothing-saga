2e5fdd2f87cd5f3a03681326bf044aa95f7445fd
Greg Phillips <greg-phillips@live.co.uk>
2020-05-27 18:02:20 -0700

    Updated firebase to now store our collections, removed dependance on shop.data.js (static file), added firebase util func to get collections from store, added this data to redux

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
-
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -37,8 +36,8 @@ class App extends React.Component {
       } else {
         setCurrentUser(userAuth);
       }
-    })
-  }
+    });
+  };
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
